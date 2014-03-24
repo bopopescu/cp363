c7a606ab91d369de850eff4fd92e71461d8ffba5
mmtaki <moha7220@mylaurier.ca>
2014-03-24 17:49:11 -0400

    Added entity classes

---
+++ FinalProject/src/Entities.py
@@ -0,0 +1,176 @@
+'''
+This class contains all entity classes related to the system
+Includes Car, Customer, Employee, Expense, Supplier classes
+
+@author: Mohamed Mohamedtaki
+'''
+class Car:
+    def __init__(self, vin, make, model, year, colour, sold, price):
+        """
+        Creates a car object
+        """
+        self._vin = vin
+        self._make = make
+        self._model = model
+        self._year = year
+        self._colour = colour
+        self._sold = sold
+        self._price = price
+    def getVin(self):
+        return self._vin
+    def getMake(self):
+        return self._make
+    def getModel(self):
+        return self._model
+    def getYear(self):
+        return self._year
+    def getColour(self):
+        return self._colour
+    def isSold(self):
+        return self._sold
+    def getPrice(self):
+        return self._price
+    def __str__(self):
+        result = """VIN: {}
+        MAKE: {}
+        MODEL: {}
+        YEAR: {}
+        COLOUR: {}
+        SOLD: {}
+        PRICE: ${}
+        """.format(self._cId, self._cName, self._joinDate, self._phone)
+        return result
+
+class Customer:
+    def __init__(self, cid, name, date, phone):
+        """
+        Creates a customer object
+        """
+        self._cId = cid
+        self._cName = name
+        self._joinDate = date
+        self._phone = phone
+    def getId(self):
+        return self._cId
+    def getName(self):
+        return self._cName
+    def getJoinDate(self):
+        return self._joinDate
+    def getPhone(self):
+        return self._phone
+    def __str__(self):
+        result = """ID: {}
+        NAME: {}
+        DATE JOINED: {}
+        PHONE: {}
+        """.format(self._cId, self._cName, self._joinDate, self._phone)
+        return result
+
+class Employee:
+    def __init__(self, eid, name, salary, dateEmployed, dateLeft, isManager):
+        """
+        Creates an Employee object
+        """
+        self._eId = eid
+        self._name = name
+        self._salary = salary
+        self._dateEmployed = dateEmployed
+        self._dateLeft = dateLeft
+        self._isManager = isManager
+    def getId(self):
+        return self._eId
+    def getName(self):
+        return self._name
+    def getSalary(self):
+        return self._salary
+    def getDateEmployed(self):
+        return self._dateEmployed
+    def getDateLeft(self):
+        return self._dateLeft
+    def setDateLeft(self, date):
+        self._dateLeft = date
+        return
+    def addToSalary(self, amount):
+        self._salary = self._salary + amount
+        return
+    def removeFromSalary(self, amount):
+        self._salary = self._salary - amount
+        return
+    def __str__(self):
+        result = """ID: {}
+        NAME: {}
+        SALARY: ${}
+        DATE OF EMPLOYMENT: {}
+        DATE OF DEPARTURE: {}
+        IS MANAGER: {}
+        """.format(self._eId, self._name, self._salary,\
+                   self._dateEmployed, self._dateLeft, self._isManager)
+        return result
+
+class Expense:
+    def __init__(self, xid, date, cost, details):
+        """
+        Creates an Expense object
+        """
+        self._xId = xid
+        self._date = date
+        self._cost = cost
+        self._details = details
+    def getId(self):
+        return self._xId
+    def getDate(self):
+        return self._date
+    def getCost(self):
+        return self._cost
+    def getDetails(self):
+        return self._details
+    def __str__(self):
+        result = """ID: {}
+        DATE: {}
+        COST: ${}
+        DETAILS:
+        {}
+        """.format(self._xId, self._date, self._cost, self._details)
+        return result
+
+class Supplier:
+    def __init__(self, sid, name, phone, street, city, province, country, postalcode):
+        """
+        Creates a Supplier object
+        """
+        self._sId = sid
+        self._sName = name
+        self._phone = phone
+        self._street = street
+        self._city = city
+        self._province = province
+        self._country = country
+        self._postalCode = postalcode
+    def getId(self):
+        return self._sId
+    def getName(self):
+        return self._sName
+    def getPhone(self):
+        return self._phone
+    def getStreet(self):
+        return self._street
+    def getCity(self):
+        return self._city
+    def getProvince(self):
+        return self._province
+    def getCountry(self):
+        return self._country
+    def getPostalCode(self):
+        return self._postalCode
+    def getAddress(self):
+        return """{}
+        {}, {}
+        {} {}
+        """.format(self._street, self._city, self._province, self._country, self._postalCode)
+    def __str__(self):
+        result = """ID: {}
+        NAME: {}
+        PHONE: {}
+        ADDRESS: {}
+        """.format(self._sId, self._sName, self._phone, self.getAddress())
+        return result
